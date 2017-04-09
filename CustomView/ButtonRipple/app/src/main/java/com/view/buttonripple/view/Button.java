0284a8f12e32c0bdf42fbb24716220ac8c8e9eca
Dong <Dong>
2017-04-10 00:09:40 +0800

    涟漪Button

---
+++ CustomView/ButtonRipple/app/src/main/java/com/view/buttonripple/view/Button.java
@@ -0,0 +1,17 @@
+package com.view.buttonripple.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by Dong on 2017/4/9.
+ */
+
+public class Button extends CustomView{
+
+    public Button(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
---
+++ CustomView/ButtonRipple/app/src/main/java/com/view/buttonripple/view/ButtonRipple.java
@@ -0,0 +1,8 @@
+package com.view.buttonripple.view;
+
+/**
+ * Created by Dong on 2017/4/9.
+ */
+
+public class ButtonRipple {
+}
