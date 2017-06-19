b13dee23c51cd99b88d6b4e20cee8eb377505484
DongOP <donking0030@gmail.com>
2017-06-19 19:06:53 +0800

    密码EditText

---
+++ PassWordEditText/app/src/main/java/com/eagle/psdedittext/MainActivity.java
@@ -0,0 +1,24 @@
+package com.eagle.psdedittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eagle.psdedittext.view.PasswordExitText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PasswordExitText mPsdText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+        mPsdText = (PasswordExitText) findViewById(R.id.psd_ed);
+    }
+}
