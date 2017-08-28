ef0dd70e412eed131685de3de5e014d1357a1f37
DongOP <donking0030@gmail.com>
2017-08-28 17:47:08 +0800

    兼容 4.4外置存储路径读取

--- TestExternalStorage/app/src/main/java/com/dong/externalstorage/util/StorageUtils.java
+++ TestExternalStorage/app/src/main/java/com/dong/externalstorage/util/StorageUtils.java
@@ -5,8 +5,10 @@ import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
 import android.os.storage.StorageManager;
+import android.util.Log;
 
 import java.io.File;
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.text.DecimalFormat;
@@ -70,64 +72,174 @@ public class StorageUtils {
         return fileSizeString;
     }
 
-    // 获取6.0以上设备的SD卡路径
-    public static String getStoragePath(Context context, boolean isUsb){
+    // 获取 设备的SD卡、U盘路径
+    public static String getStoragePath(Context context, boolean isUsb) {
+        String path = "";
 
-        String path="";
+        if (Build.VERSION.SDK_INT < 23) {
+            String pathKey = "";
+            if (isUsb) {
+                pathKey = "usb";
+                // 小于 Android 6.0 版本的U盘路径
+                path = getStoragePathOld(context, pathKey);
+            } else {
+                pathKey = "sd";
+                // 小于 Android 6.0 版本的sd路径
+                path = getSDFilePath(context);
+            }
+            Log.e(TAG, "6.0以下...getStoragePath.....path=" + path);
 
-        if(Build.VERSION.SDK_INT<23)return path;
+            return path;
+        } else {
+            StorageManager mStorageManager = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
+            Class<?> volumeInfoClazz = null;
+            Class<?> diskInfoClaszz = null;
 
-         StorageManager mStorageManager = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
-         Class<?> volumeInfoClazz=null;
-         Class<?> diskInfoClaszz=null;
+            try {
+                volumeInfoClazz = Class.forName("android.os.storage.VolumeInfo");
+                diskInfoClaszz = Class.forName("android.os.storage.DiskInfo");
 
-         try {
-             volumeInfoClazz = Class.forName("android.os.storage.VolumeInfo");
-             diskInfoClaszz = Class.forName("android.os.storage.DiskInfo");
+                Method StorageManager_getVolumes = Class.forName("android.os.storage.StorageManager").getMethod("getVolumes");
+                Method VolumeInfo_GetDisk = volumeInfoClazz.getMethod("getDisk");
+                Method VolumeInfo_GetPath = volumeInfoClazz.getMethod("getPath");
+                Method DiskInfo_IsUsb = diskInfoClaszz.getMethod("isUsb");
+                Method DiskInfo_IsSd = diskInfoClaszz.getMethod("isSd");
 
-             Method StorageManager_getVolumes=Class.forName("android.os.storage.StorageManager").getMethod("getVolumes");
-             Method VolumeInfo_GetDisk = volumeInfoClazz.getMethod("getDisk");
-             Method VolumeInfo_GetPath = volumeInfoClazz.getMethod("getPath");
-             Method DiskInfo_IsUsb = diskInfoClaszz.getMethod("isUsb");
-             Method DiskInfo_IsSd = diskInfoClaszz.getMethod("isSd");
-
-             List<Object> List_VolumeInfo = (List<Object>) StorageManager_getVolumes.invoke(mStorageManager);
-             for(int i=0; i<List_VolumeInfo.size(); i++){
-                 Object volumeInfo = List_VolumeInfo.get(i);
-                 Object diskInfo = VolumeInfo_GetDisk.invoke(volumeInfo);
+                List<Object> List_VolumeInfo = (List<Object>) StorageManager_getVolumes.invoke(mStorageManager);
+                for (int i = 0; i < List_VolumeInfo.size(); i++) {
+                    Object volumeInfo = List_VolumeInfo.get(i);
+                    Object diskInfo = VolumeInfo_GetDisk.invoke(volumeInfo);
 
 
-                 if(diskInfo==null)continue;
+                    if (diskInfo == null) continue;
 
-                 boolean sd= (boolean) DiskInfo_IsSd.invoke(diskInfo);
-                 boolean usb= (boolean) DiskInfo_IsUsb.invoke(diskInfo);
+                    boolean sd = (boolean) DiskInfo_IsSd.invoke(diskInfo);
+                    boolean usb = (boolean) DiskInfo_IsUsb.invoke(diskInfo);
 
-                 File file= (File) VolumeInfo_GetPath.invoke(volumeInfo);
-                 // Logger.d("diskinfo="+file.getAbsolutePath()+"; is_usb="+usb+";  is_sd="+sd);
+                    File file = (File) VolumeInfo_GetPath.invoke(volumeInfo);
+                    // Logger.d("diskinfo="+file.getAbsolutePath()+"; is_usb="+usb+";  is_sd="+sd);
 
-                 if(isUsb == usb){//usb
-                     if (null != file) {
-                         path=file.getAbsolutePath();
-                     }
-                     break;
-                 }else if(!isUsb == sd){//sd
-                     if (null != file) {
-                         path=file.getAbsolutePath();
-                     }
-                     break;
-                 }
+                    if (isUsb == usb) {//usb
+                        if (null != file) {
+                            path = file.getAbsolutePath();
+                        }
+                        break;
+                    } else if (!isUsb == sd) {//sd
+                        if (null != file) {
+                            path = file.getAbsolutePath();
+                        }
+                        break;
+                    }
 
-             }
+                }
 
-         } catch (ClassNotFoundException e) {
-             e.printStackTrace();
-         } catch (NoSuchMethodException e) {
-             e.printStackTrace();
-         } catch (InvocationTargetException e) {
-             e.printStackTrace();
-         } catch (IllegalAccessException e) {
-             e.printStackTrace();
-         }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            Log.e(TAG, "6.0...getStoragePath.....path=" + path);
+
+            return path;
+        }
+    }
+
+    /**
+     * 获取6.0以下机器的外置U盘路径
+     *
+     * @param keyPath extsd 外置SD卡，usb 外置U盘
+     * @return 返回外置SD卡或者U盘的路径
+     */
+    public static String getStoragePathOld(Context context, String keyPath) {
+        Method mMethodGetPaths = null;
+        String[] paths = null;
+        String state = null;
+        String path = null;
+        StorageManager mStorageManager = (StorageManager) context.getSystemService(context.STORAGE_SERVICE);
+
+        if (Build.VERSION.SDK_INT < 23) {
+            try {
+                mMethodGetPaths = mStorageManager.getClass().getMethod(
+                        "getVolumePaths");
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (mMethodGetPaths != null) {
+                    paths = (String[]) mMethodGetPaths.invoke(mStorageManager);
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            mMethodGetPaths = null;
+            for (int i = 0; i < paths.length; i++) {
+                // Utils.loge(TAG, "getStoragePathOld...paths[" + i + "]="+ paths[i]);
+                if (paths[i].contains(keyPath)) {
+                    try {
+                        mMethodGetPaths = mStorageManager.getClass().getMethod(
+                                "getVolumeState", String.class);
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (mMethodGetPaths != null) {
+                            state = (String) mMethodGetPaths.invoke(mStorageManager, paths[i]);
+                        }
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                    if (Environment.MEDIA_MOUNTED.equals(state)) {
+                        path = paths[i];
+                    }
+                }
+            }
+        }
         return path;
     }
+
+    /**
+     * 获取 Android 4.4 外置SD卡路径
+     */
+    public static String getSDFilePath(Context context) {
+        StorageManager mStorageManager = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
+        Class<?> storageVolumeClazz = null;
+        try {
+            storageVolumeClazz = Class.forName("android.os.storage.StorageVolume");
+            Method getVolumeList = mStorageManager.getClass().getMethod("getVolumeList");
+            Method getPath = storageVolumeClazz.getMethod("getPath");
+            Method isRemovable = storageVolumeClazz.getMethod("isRemovable");
+            Object result = getVolumeList.invoke(mStorageManager);
+            final int length = Array.getLength(result);
+            for (int i = 0; i < length; i++) {
+                Object storageVolumeElement = Array.get(result, i);
+                String path = (String) getPath.invoke(storageVolumeElement);
+                boolean removable = (Boolean) isRemovable.invoke(storageVolumeElement);
+                if (removable) {
+                    return path;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
