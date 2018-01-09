ab2d14289c15bd05c9a80919fb17b4ea78b25338
Dong <donking0030@gmail.com>
2018-01-09 18:13:00 +0800

    Update BubbleSort.java

--- AlgorithmLearning/untitled/src/com/dong/untitled/util/BubbleSort.java
+++ AlgorithmLearning/untitled/src/com/dong/untitled/util/BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort {
     public static void bubbleSort(int[] arr) {
         int temp;
         for (int i = 0; i < arr.length - 1; i++) { // 趟数
-            for (int j = 0; j < arr.length - i - 1; j++) {
+            for (int j = 0; j < arr.length - i - 1; j++) { // j是在第i趟可交换的次数，j从0开始（0-->arr.length - i - 1)
                 // 大的数字往后移换
                 if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
