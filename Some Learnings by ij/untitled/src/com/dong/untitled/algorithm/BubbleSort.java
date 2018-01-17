715f41bf6c05a0238994cc8a28816e211e6f223e
DongOP <donking0030@gmail.com>
2018-01-17 14:22:33 +0800

    add note

--- Some Learnings by ij/untitled/src/com/dong/untitled/algorithm/BubbleSort.java
+++ Some Learnings by ij/untitled/src/com/dong/untitled/algorithm/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort {
     public static void bubbleSort(int[] arr) {
         int temp;
         for (int i = 0; i < arr.length - 1; i++) { // 趟数
-            for (int j = 0; j < arr.length - i - 1; j++) {
+            for (int j = 0; j < arr.length - i - 1; j++) { // j表示：第i趟，元素可进行的交换次数
                 // 大的数字往后移换
                 if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
